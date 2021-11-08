45eb4b9cbca0d67e21eb72fe145d45fb7273efa7
Kishan Akbari <88237282+kishanakbari8888@users.noreply.github.com>
2021-11-08 09:58:59 +0530

    Add files via upload

---
+++ code4.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#include <stdio.h>
+#include "string.h"
+using namespace std;
+#define int long long 
+#define endl '\n' 
+#define pb push_back
+#define all(x) x.begin(),x.end() 
+#define con(s1,s2) strstr(s1.c_str(),s2.c_str())
+#define foreach(x,y) for(auto &x:y)
+#define allr(x) x.rbegin(),x.rend()
+#define sum(a) accumulate(a.begin(), a.end(), (int)0)
+#define ff first
+#define ss second
+#define pi pair<int,int>
+int M = 998244353;
+
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout<<setprecision(10)<<fixed;
+
+    int t; cin>>t;
+    while(t--)
+    {
+                
+    }    
+
+
+
+    return 0;
+}
